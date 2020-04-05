9ec12b952bc74f62f6e7d800984317492a257742
Rolly Falco Villacacan <32959267+funsideofwine@users.noreply.github.com>
2020-04-05 11:34:43 +0300

    fix timing

--- chart3.php
+++ chart3.php
@@ -5,26 +5,35 @@
   //$legend_time_name =  "Time";
   //$number_of_samples =  "1000";
   //$interval_span =  "30"; //mins
+  $t_span = date("i",time()); //minute
+  //$m = 15; //minute
 
   date_default_timezone_set("Asia/Riyadh");
 
+	///echo $t_span;
 ?>
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script language="JavaScript" type="text/javascript">
+	//setTimeout("location.href = 'chart3.php?t_span=<?php echo $t_span; ?>'",15000); // milliseconds, so 10 seconds = 10000ms
+	setTimeout("location.href = 'chart3.php?t_span=<?php echo $t_span; ?>'", 1 * 60 * 1000 ); // milliseconds, so 10 seconds = 10000ms
 </script>
 
+</script>
+<div id="curve_chart" style="width: 900px; height: 500px">
 <script type="text/javascript">
 google.charts.load('current', {
   callback: function () {
 
     drawChart();
-    //setInterval(drawChart, (15 * 60 * 1000));
-    setInterval(drawChart, (5000));
+    setInterval(drawChart, (1 * 60 * 1000));
+    //setInterval(drawChart, (5000));
 
     function drawChart() {
       $.ajax({
-        url: 'fetch_3.php',
+        url: 'fetch_3.php?t_span=<?php echo $t_span; ?>',
         type: 'get',
 		datatype: 'json',
         success: function (txt) {
@@ -33,21 +42,34 @@ google.charts.load('current', {
             txt = txt.substring(0, txt.length - 1);
           }
 
-          var txtData = JSON.parse('[["Datetime", "Temp ºC", "Humidity %"],' + txt + ']');
+          var txtData = JSON.parse('[["number", "Temp ºC", "Humidity %"],' + txt + ']');
           var data = google.visualization.arrayToDataTable(txtData);
 
           var options = {
+
 			title: 'Date/Time: <?php echo date('h A ( M-d-Y )',time()); ?>',
             curveType: 'function',
-            hAxis: {
-              title: 'Last 30 Minutes',
+
+			hAxis: {
+              title: 'Last 15 Minutes',
               //direction: '-1'
             },
-            legend: { position: 'bottom' }
+
+			vAxis: {
+              title: 'Values',
+              //direction: '-1'
+            },
+
+			legend: { position: 'bottom' }
           };
 
+          //var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
           var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
           chart.draw(data, options);
+
+		//var chart = new google.charts.Line(document.getElementById('curve_chart'));
+		//chart.draw(data, google.charts.Line.convertOptions(options));
+
         }
       });
     }
@@ -57,5 +79,5 @@ google.charts.load('current', {
 });
 </script>
 
-<div id="curve_chart" style="width: 900px; height: 500px">
+
 </div>
--- fetch_3.php
+++ fetch_3.php
@@ -1,10 +1,20 @@
 <?php
-  $title = "Temperature (ºC) / Humidity Graph (%)"; //degree symbol Alt+0186
-  $legend_sensor_name1 = "Temperature (ºC)";
-  $legend_sensor_name2 = "Humidity (%)";
-  $legend_time_name =  "Time";
-  $number_of_samples =  "1000";
-  $interval_span =  "30"; //mins
+
+
+	if(isset($_GET['t_span'])) $t_span = $_GET['t_span'];
+	else $t_span = 0;
+
+	$title = "Temperature (ºC) / Humidity Graph (%)"; //degree symbol Alt+0186
+	$legend_sensor_name1 = "Temperature (ºC)";
+	$legend_sensor_name2 = "Humidity (%)";
+	$legend_time_name =  "Time";
+	$number_of_samples =  "1000";
+	$interval_span =  "45"; //mins
+
+	if($t_span > $interval_span) $interval_span;
+	else $interval_span = $t_span;
+
+	//echo $t_span;
 ?>
 
 
@@ -40,16 +50,43 @@
       }
 
 
+					//$time = (int) date("i",$row['dtime']);
+					$y = date("Y",time()); // year
+					$mo = date("m",time()); //month
+					$d = date("d",time()); //day
+					////
+					$h = date("H",time()); // hour
+					//$h_end = date("H",time()); // hour
+					//$h_start = date("H",time()); // hour
+					$m = date("i",time()); //minute
+
+
+
+					//if($m > $interval_span){
+					//	$m_start = date("i",time()) - $interval_span; //minute
+					//}
+					//else {
+					//	$m_start = (date("i",time()) + 60) - $interval_span; //minute
+					//	$h_start = date("H",time()) - 1; // hour
+					//}
+					//
+					//$m_end = date("i",time()); //minute
+					//$s = date("s",time()); //minute
+
       //$sql = " SELECT *  FROM tbl_sensors ORDER BY id DESC LIMIT $number_of_samples ";
       //$sql = " SELECT *  FROM tbl_sensors WHERE dtime BETWEEN '".date("Y-m-d H:i:s",strtotime("-30 minutes"))."' AND '".date("Y-m-d H:i:s",strtotime("now"))."' ORDER BY id DESC LIMIT $number_of_samples ";
       //$sql = " SELECT *  FROM tbl_sensors WHERE dtime BETWEEN '".date("Y-m-d H:i:s",strtotime("-$interval_span minutes"))."' AND '".date("Y-m-d H:i:s",strtotime("now"))."' ORDER BY id DESC "; // LIMIT $number_of_samples ";
       $sql = " SELECT *  FROM tbl_sensors WHERE dtime BETWEEN '".date("Y-m-d H:i:s",strtotime("-$interval_span minutes"))."' AND '".date("Y-m-d H:i:s",strtotime("now"))."' ORDER BY id DESC LIMIT $number_of_samples ";
+      //$sql = " SELECT *  FROM tbl_sensors WHERE dtime BETWEEN '".date("Y-m-d H:i:s",strtotime("-$interval_span minutes"))."' AND '".date("Y-m-d H:i:s",strtotime("-4 minutes"))."' ORDER BY id DESC LIMIT $number_of_samples ";
+      //$sql = " SELECT *  FROM tbl_sensors WHERE dtime BETWEEN '".date("Y-m-d H:i:s",strtotime("-29 minutes"))."' AND '".date("Y-m-d H:i:s",strtotime("now"))."' ORDER BY id DESC LIMIT $number_of_samples ";
+      //$sql = " SELECT *  FROM tbl_sensors ORDER BY id DESC LIMIT $number_of_samples ";
       $result = $conn->query($sql);
 
       if ($result->num_rows > 0) {
-		$i = 0;
+		//$i = 0;
+		//$m_prev = 0;
         while($row = $result->fetch_assoc()) {
-					$i++;
+					///$i++;
 					$time = strtotime($row['dtime']);
 					//$time = (int) date("i",$row['dtime']);
 					//$y = date("Y",$time); // year
@@ -66,8 +103,15 @@
 					$hum = $row['hum'];
 					//$id = $row['id'];
 
+					//$date = date("Y-m-d H:i:s",$time)
+
+					//if($m < $interval_span) continue;
 					if($temp == 0) continue;
 					if($hum == 0) continue;
+
+					//if($m == $m_prev) continue;
+					//else $m = $m_prev;
+
 					//echo '[15, 106],[14, 105],[13, 110],[12, 119],[11, 107],[10, 132],[9, 121],[8, 111],[7, 126],[6, 115],[5, 109],';
 					//echo "[$i, $hum, $temp],";
 					echo "[$m, $hum, $temp],";
